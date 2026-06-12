a5f245bdd174e51784e436351383ab36ec1b64d9
Yael <243678@ids.upchiapas.edu.mx>
2026-06-11 18:58:03 -0600

    refactor: rearranged folders to have the colorscheme added automatically
    i think

---
+++ lua/palette.lua
@@ -0,0 +1,19 @@
+local colors = {
+	bg = "#03102c",
+	bg_unfocused = "#000b20",
+	fg = "#ff2e97",
+	fg_unfocused = "#b00060",
+	surface = "#0e0952",
+	primary_txt = "#ffffff",
+	functions = "#ffd400", --amarillo
+	keywords = "#ff2cf1", --magenta
+	strings = "#0ef3ff", --cian
+	comments = "#0098df", --azulito
+	visual = "#052355",  --azul oscurillo
+	cursorLine = "#1C1347", --azul más oscuro
+	special = "#7CD492",
+	error = "#AC077A",
+	warn = "#FCE094"
+}
+
+return colors
